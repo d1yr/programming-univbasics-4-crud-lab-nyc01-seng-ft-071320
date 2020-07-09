9a3148c8ad633f43ab20a9c9885115a04d4ae72b
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-09 15:38:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   myarray = ['apples','oranges','bananas','cherries']
- myarray.push('pears')
+ myarray << 'pears'
  end
 
 def add_element_to_start_of_array(array, element)
