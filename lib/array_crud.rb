d74d4f42f8804a0a5a179adbc0d3fe51c6e6d0d2
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-09 15:43:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  my_array = Array.new 
+  my_array[]
 end
 
 def retrieve_element_from_index(array, index_number)
